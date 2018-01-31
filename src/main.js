55cdb39fa3451a8ceab1668de711a7f51ac54dc7
zhyjor <zhyjor@163.com>
2018-01-31 18:15:35 +0800

    fixed

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+// import Vue from 'vue'
+// import App from './App'
 
-Vue.config.productionTip = false
+// // Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+//  // eslint-disable no-new
+// new Vue({
+//   el: '#app',
+//   template: '<App/>',
+//   components: { App }
+// });
+
+import VueMarquee from './Marquee.vue'
+export default VueMarquee;
+
+// var VueMarquee = require('./Marquee.vue');
+// module.exports = VueMarquee
